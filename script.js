7fd7c6bb58011031b0cc380ae8d2dde33e169514
lazaro moyano <140737385+lazzmoyano@users.noreply.github.com>
2023-12-04 18:39:01 -0300

    commit changes

---
+++ script.js
@@ -0,0 +1,13 @@
+function enableDarkMode() {
+  let main_body = document.body;
+  main_body.classList.toggle("dark-mode");
+
+
+  if (document.getElementById('checkitem').checked) {
+      console.log("checked");
+  } else {
+      console.log("NOT checked");
+  }
+
+
+}
